06e6e89c5b1889914f01c2a5cf3689c510685df1
Deepa Paneru <deepapaneru220@gmail.com>
2025-12-13 21:40:02 +0545

    portfolio JS
    Refactor contact form handling and smooth scrolling logic.

--- script.js
+++ script.js
@@ -54,42 +54,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 //conatct-form
-    const contactForm = document.querySelector('.contact form');
-    if (contactForm) {
-        contactForm.addEventListener('submit', function(e) {
-            let isValid = true;
-             const nameInput = contactForm.querySelector('input[type="text"][placeholder=" Name"]');
-            const emailInput = contactForm.querySelector('input[type="email"]');
-            const messageInput = contactForm.querySelector('textarea');
- // Simple validation checks
-            if (nameInput.value.trim() === '') {
-                alert('Please enter your name.');
-                isValid = false;
-            } else if (emailInput.value.trim() === '') {
-                alert('Please enter your email.');
-                isValid = false;
-            } else if (!isValidEmail(emailInput.value.trim())) {
-                alert('Please enter a valid email address.');
-                isValid = false;
-            } else if (messageInput.value.trim() === '') {
-                alert('Please enter a message.');
-                isValid = false;
-            }
-             if (!isValid) {
-                e.preventDefault(); // Stop form submission if invalid
-            } else { 
-                 e.preventDefault();
-                alert('an error occured!');
+    function clearForm() {
+    document.getElementById("contactForm").reset();
+}
+
             
-                contactForm.reset(); // Clear the form
-            }
-        });
-    }
-     function isValidEmail(email) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    }
      
 });
-
 
